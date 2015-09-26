3cef510b6231d039d7354a700196ad88fa62212c
Syrus Mesdaghi <syrusm@gmail.com>
2015-09-26 09:33:34 -0400

    added support file upload

--- vida/fileservice/api.py
+++ vida/fileservice/api.py
@@ -1,6 +1,8 @@
 from tastypie.authentication import BasicAuthentication
+from tastypie.authorization import Authorization
 from tastypie.utils import trailing_slash
 from tastypie.bundle import Bundle
+from django.core.urlresolvers import NoReverseMatch
 from tastypie.exceptions import NotFound
 from tastypie.resources import Resource
 from django.conf.urls import url
@@ -18,12 +20,14 @@ class FileItem(object):
 class FileItemResource(Resource):
     id = fields.IntegerField(attribute='id')
     name = fields.CharField(attribute='name')
+    file = fields.FileField(attribute='file', null=True, blank=True)
 
     class Meta:
         resource_name = 'fileitem'
         object_class = FileItem
         fields = ['name']
         authentication = BasicAuthentication()
+        authorization = Authorization()
 
     def determine_format(self, request):
         return 'application/json'
@@ -38,6 +42,7 @@ class FileItemResource(Resource):
             file_item.id = fake_id
             fake_id += 1
             file_item.name = name
+            file_item.file = helpers.get_filename_absolute(name)
             file_items.append(file_item)
         return file_items
 
@@ -57,22 +62,25 @@ class FileItemResource(Resource):
             if file_item.name == helpers.u_to_str(name):
                 return file_item
 
+    def deserialize(self, request, data, format=None):
+        if not format:
+            format = request.META.get('CONTENT_TYPE', 'application/json')
 
-    # adapted this from ModelResource
-    def get_resource_uri(self, bundle_or_obj):
-        kwargs = {
-            'resource_name': self._meta.resource_name,
-        }
+        if format == 'application/x-www-form-urlencoded':
+            return request.POST
 
+        if format.startswith('multipart'):
+            data = request.POST.copy()
+            data.update(request.FILES)
+            return data
+
+        return super(Resource, self).deserialize(request, data, format)
+
+    def detail_uri_kwargs(self, bundle_or_obj):
         if isinstance(bundle_or_obj, Bundle):
-            kwargs['pk'] = bundle_or_obj.obj.id # pk is referenced in ModelResource
+            return {'name': bundle_or_obj.obj.name}
         else:
-            kwargs['pk'] = bundle_or_obj.id
-
-        if self._meta.api_name is not None:
-            kwargs['api_name'] = self._meta.api_name
-
-        return self._build_reverse_url('api_dispatch_detail', kwargs = kwargs)
+            return {'name': bundle_or_obj.name}
 
     def get_object_list(self, request):
         # inner get of object list... this is where you'll need to
@@ -90,6 +98,16 @@ class FileItemResource(Resource):
             return FileItemResource.get_file_item(kwargs)
         except KeyError:
             raise NotFound("Object not found")
+
+    def obj_create(self, bundle, request=None, **kwargs):
+        # create a new File
+        bundle.obj = FileItem()
+        # full_hydrate does the heavy lifting mapping the
+        # POST-ed payload key/values to object attribute/values
+        bundle = self.full_hydrate(bundle)
+        with open(helpers.get_filename_absolute(bundle.data[u'name']), 'w+') as uploaded_file:
+            uploaded_file.write(bundle.data[u'file'])
+        return bundle
 
     def prepend_urls(self):
         """ Add the following array of urls to the resource base urls """
