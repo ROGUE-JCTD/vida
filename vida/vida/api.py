ba15e75bd07e388465b657d16aa03d0d062b31e2
Syrus Mesdaghi <syrusm@gmail.com>
2015-09-28 10:55:09 -0400

    allow filtering on related fields

--- vida/vida/api.py
+++ vida/vida/api.py
@@ -1,4 +1,4 @@
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, ALL
 from tastypie.authentication import BasicAuthentication
 from tastypie.authorization import Authorization
 from tastypie import fields
@@ -23,6 +23,14 @@ class PersonResource(ModelResource):
         excludes = ['start_date', 'stop_date']
         authentication = BasicAuthentication()
         authorization = Authorization()
+        filtering = {
+            'family_name': ALL,
+            'given_name': ALL,
+            'mothers_given_name': ALL,
+            'fathers_given_name': ALL,
+            'description': ALL,
+            'barcode': ALL,
+        }
 
     def determine_format(self, request):
         return 'application/json'
