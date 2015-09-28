f35cd86ef3ee1e19becd3874e9f69bff4ba58129
Syrus Mesdaghi <syrusm@gmail.com>
2015-09-28 18:16:25 -0400

    added a custom filter

--- vida/vida/api.py
+++ vida/vida/api.py
@@ -3,6 +3,7 @@ from tastypie.authentication import BasicAuthentication
 from tastypie.authorization import Authorization
 from tastypie import fields
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 
 from vida.vida.models import Person
 from vida.vida.models import Shelter
@@ -19,6 +20,19 @@ class PersonResource(ModelResource):
     created_by = fields.ToOneField(UserResource, 'created_by',  full=True, blank=True, null=True)
 
     class Meta:
+        def filter_custom_age(url_args):
+            """Build Custom filter to match age between a max and min if it is set"""
+            try:
+                age = int(url_args.pop('age_about') [0])
+            except KeyError:
+                return None
+
+            age_query = (
+                Q(age__gte=age-10) &
+                Q(age__lte=age+10)
+            )
+            return age_query
+
         queryset = Person.objects.all()
         excludes = ['start_date', 'stop_date']
         authentication = BasicAuthentication()
@@ -30,10 +44,41 @@ class PersonResource(ModelResource):
             'fathers_given_name': ALL,
             'description': ALL,
             'barcode': ALL,
+            'age': ALL,
         }
+        custom_filters = {'age_about': filter_custom_age}
 
     def determine_format(self, request):
         return 'application/json'
+
+    def build_filters(self, filters=None):
+        """Allow for building of custom filters based on url keyword."""
+        custom_filters = self._meta.custom_filters
+        custom_queries = {}
+
+        for filter_id, filter_method in custom_filters.iteritems():
+            built_filter = filter_method(filters)
+            if built_filter:
+                custom_queries[filter_id] = built_filter
+
+        orm_filters = super(PersonResource, self).build_filters(filters)
+        for query_id, query in custom_queries.iteritems():
+            orm_filters[query_id] = query
+        return orm_filters
+
+    def apply_filters(self, request, applicable_filters):
+        """Allow for the application of custom filters built in the build_filters method"""
+        custom_built_queries = [filter_id for filter_id in self._meta.custom_filters.keys()]
+        post_filters = []
+        for key in applicable_filters.keys():
+            if key in custom_built_queries:
+                post_filters.append(applicable_filters.pop(key))
+
+        filtered = super(PersonResource, self).apply_filters(request, applicable_filters)
+        for post_filter in post_filters:
+            filtered = filtered.filter(post_filter)
+
+        return filtered
 
 
 class ShelterResource(ModelResource):
