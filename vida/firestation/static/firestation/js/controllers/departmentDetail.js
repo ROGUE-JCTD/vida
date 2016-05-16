2b2592a6998b9bfc63e948beb2342862b657a4fb
Bird, Samuel S <sam.bird.dev@gmail.com>
2016-05-16 13:21:13 -0400

    Fixed shelter detail marker as well

--- vida/firestation/static/firestation/js/controllers/departmentDetail.js
+++ vida/firestation/static/firestation/js/controllers/departmentDetail.js
@@ -13,47 +13,46 @@
           var layersControl = L.control.layers().addTo(departmentMap);
 
           if (showStations) {
-              FireStation.query({department: config.id}).$promise.then(function(data) {
-                 $scope.stations = data.objects;
+            FireStation.query({department: config.id}).$promise.then(function(data) {
+              $scope.stations = data.objects;
 
-                  var stationMarkers = [];
-                  var numFireStations = $scope.stations.length;
-                  for (var i = 0; i < numFireStations; i++) {
-                      var station = $scope.stations[i];
-                      var marker = L.marker(station.geom.coordinates.reverse(), {icon: stationIcon});
-                      marker.bindPopup('<b>' + station.name + '</b><br/>' + station.address + ', ' + station.city + ' ' +
-                          station.state);
-                      stationMarkers.push(marker);
-                  }
+              var stationMarkers = [];
+              var numFireStations = $scope.stations.length;
+              for (var i = 0; i < numFireStations; i++) {
+                  var station = $scope.stations[i];
+                  var marker = L.marker(station.geom.coordinates.reverse(), {icon: stationIcon});
+                  marker.bindPopup('<b>' + station.name + '</b><br/>' + station.address + ', ' + station.city + ' ' +
+                      station.state);
+                  stationMarkers.push(marker);
+              }
 
-				 if (numFireStations > 0) {
-					var stationLayer = L.featureGroup(stationMarkers);
-	
-					// Uncomment to show Fire Stations by default
-					// stationLayer.addTo(departmentMap);
-	
-					layersControl.addOverlay(stationLayer, 'Fire Stations');
-	
-					if (config.geom === null) {
-						departmentMap.fitBounds(stationLayer.getBounds(), fitBoundsOptions);
-					}
-			     }
-              });
+              if (numFireStations > 0) {
+                var stationLayer = L.featureGroup(stationMarkers);
+
+                // Uncomment to show Fire Stations by default
+                // stationLayer.addTo(departmentMap);
+
+                layersControl.addOverlay(stationLayer, 'Fire Stations');
+
+                if (config.geom === null) {
+                  departmentMap.fitBounds(stationLayer.getBounds(), fitBoundsOptions);
+                }
+              }});
           }
 
           if (config.centroid != null) {
-           var headquarters = L.marker(config.centroid, {icon: headquartersIcon,zIndexOffset:1000});
-           headquarters.addTo(departmentMap);
-           layersControl.addOverlay(headquarters, 'Headquarters Location');
+           //var headquarters = L.marker(config.centroid, {icon: headquartersIcon,zIndexOffset:1000});
+           //headquarters.addTo(departmentMap);
+           //layersControl.addOverlay(headquarters, 'Headquarters Location');
           };
 
           if (config.geom != null) {
            var countyBoundary = L.geoJson(config.geom, {
-                                  style: function (feature) {
-                                      return {color: '#0074D9', fillOpacity: .05, opacity:.8, weight:2};
+                                  pointToLayer: function(feature, latlng) {
+                                    return L.marker(latlng, {icon: L.VIDAMarkers.headquartersmarker()});
                                   }
                               }).addTo(departmentMap);
-            layersControl.addOverlay(countyBoundary, 'Jurisdiction Boundary');
+            layersControl.addOverlay(countyBoundary, 'VIDA Shelter');
             departmentMap.fitBounds(countyBoundary.getBounds(), fitBoundsOptions);
           } else {
               departmentMap.setView(config.centroid, 13);
