855ee826e79815ff17c7cf3d0c4ec51f72f6e9d0
Bird, Samuel S <sam.bird.dev@gmail.com>
2016-05-16 10:53:50 -0400

    Added icon to use since default is broken on AWS

--- vida/firestation/static/firestation/js/controllers/home.js
+++ vida/firestation/static/firestation/js/controllers/home.js
@@ -17,7 +17,7 @@
           L.geoJson(newGeoJSON, {
             pointToLayer: function(feature, latlng) {
               // Don't use icon, gets default
-              return L.marker(latlng, {});
+              return L.marker(latlng, {icon: L.VIDAMarkers.headquartersmarker()});
             },
             onEachFeature: function(feature, layer) {
               var popUp = '<div><span style="padding-right: 5px;">' + feature.properties.name + '</span><a class="fa fa-chevron-right trigger" href=' + feature.properties.url + '></a></div>';
